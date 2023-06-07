b0e7008e6bc2583bf29ec04d1e3c074ead833625
Luiz F. Picolo <thaissa2608mariana@gmail.com>
2023-06-07 14:24:45 -0400

    Commit alterar

---
+++ exercicio_03.js
@@ -0,0 +1,40 @@
+class MeuErro extends Error {
+    constructor(message){
+      super(message);
+      this.name = "Meu Erro";
+    }
+  }
+
+class NerdIF {
+    constructor(estudante, cosplay, nota_cosplay) {
+      this.estudante = estudante;
+      this.cosplay = cosplay;
+      this.nota_cosplay = nota_cosplay;
+    }
+  
+    retornarAtributos() {
+        try {
+            return this.atributos();
+          } catch (error) {
+            return error
+          }
+        }
+  
+    atributos() {
+        if (this.nome != ""){
+        return {
+        estudante: this.estudante,
+        cosplay: this.cosplay,
+        nota_cosplay: this.nota_cosplay
+      };
+    }else {
+        throw new MeuErro("Mensagem de erro");
+      }
+    }
+}
+  const aluno = new NerdIF("João", "Homem-Aranha", 9.5);
+  const atributos = (aluno.retornarAtributos());
+  
+  console.log(atributos.estudante); 
+  console.log(atributos.cosplay);   
+  console.log(atributos.nota_cosplay);
